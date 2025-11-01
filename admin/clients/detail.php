abd36091ea32c4b910fa5daf5fe62d06f3736d4b
Intermedia Colombia <intermediacolombia@gmail.com>
2025-10-31 22:28:47 -0500

    Correccion Creditos Masivos

--- admin/clients/detail.php
+++ admin/clients/detail.php
@@ -462,7 +462,7 @@ table{
   <!-- SweetAlert2 JS -->
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>	
 	<!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <!-- DataTables JS -->
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script><br>
