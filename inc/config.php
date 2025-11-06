ec9e3fc0ebabcbb360e116d112a8ac4bcf7b6e09
Intermedia Colombia <intermediacolombia@gmail.com>
2025-11-06 14:46:28 -0500

    Integrar Pagos en Linea

--- inc/config.php
+++ inc/config.php
@@ -55,8 +55,8 @@ try {
 	define('SYSTEM_COLOR_SECONDARY_DARK', $settings['system_color_secondary_dark'] ?? '#000');
 	
 	/* ===== MERCADO PAGO CONFIG ===== */
-	define('MP_ACCESS_TOKEN', 'TEST-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-	define('MP_PUBLIC_KEY',  'TEST-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+	define('MP_ACCESS_TOKEN', 'TEST-5302646716770684-082811-e37ae728767c302d95eb1b7b272cf776-47181710');
+	define('MP_PUBLIC_KEY',  'TEST-277ef300-6b73-4c5f-bab3-9c2746de4294');
 
 
 } catch (PDOException $e) {
