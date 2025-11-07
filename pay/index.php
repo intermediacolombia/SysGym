052e1911d9efed1558ebf8b27eff13e7b79b912a
Intermedia Colombia <intermediacolombia@gmail.com>
2025-11-07 02:09:02 -0500

    Correccion retorno factura

--- pay/index.php
+++ pay/index.php
@@ -21,7 +21,7 @@ header('Content-Type: text/html; charset=utf-8');
 body {
   font-family: 'Poppins', sans-serif;
   /*background: #f0f3f9;*/
-background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(<?= URL_BASE;?>/admin/images/background.jpg) no-repeat center center fixed;
+background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(<?= URLBASE;?>/admin/images/background.jpg) no-repeat center center fixed;
     background-size: cover;
 }
   display: flex;
