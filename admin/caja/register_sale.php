dca939da820fcae38aa26a02c3a22fb2b784fefe
Intermedia Colombia <intermediacolombia@gmail.com>
2025-11-12 13:12:04 -0500

    Implemntacion de Alertas de stock

--- admin/caja/register_sale.php
+++ admin/caja/register_sale.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/../login/session.php';
 require_once __DIR__ . '/../../inc/config.php';
-require_once __DIR__ . '/../../whatsapp/send_ws_alert.php'; // <- Función para enviar WhatsApp
+require_once __DIR__ . '/../../../whatsapp/send_ws_alert.php'; // <- Función para enviar WhatsApp
 
 header('Content-Type: application/json');
 
