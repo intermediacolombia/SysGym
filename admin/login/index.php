590faba6d9e44c725c1e700bd6422217eae315b3
Intermedia Colombia <intermediacolombia@gmail.com>
2025-11-18 11:58:12 -0500

    Mejora Visual de los botones

--- admin/login/index.php
+++ admin/login/index.php
@@ -113,6 +113,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <!-- Font Awesome para íconos -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+	<link rel="stylesheet" href="<?php echo URLBASE; ?>/admin/css/buttons.css?cache=<?php echo time(); ?>">
   <style>
     body {
   background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("<?php echo $url;?>/admin/images/background.jpg") no-repeat center center fixed;
