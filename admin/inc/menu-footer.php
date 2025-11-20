6caf94761c812e42f8faf4c1a7b1a2aec6dab9ec
Intermedia Colombia <intermediacolombia@gmail.com>
2025-11-20 08:36:50 -0500

    Correccion busqueda clientes en creditos de caja

--- admin/inc/menu-footer.php
+++ admin/inc/menu-footer.php
@@ -102,7 +102,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   
-<!--script src="/admin/js/keepAlive.js?cache=<?php echo time();?>"></script-->
+<script src="/admin/js/keepAlive.js?cache=<?php echo time();?>"></script>
 <script src="/admin/js/theme.js?<?php echo time();?>"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <!-- jQuery -->
