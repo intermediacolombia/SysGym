be0988eda7b461c90d79cae93788d91a30f99947
Intermedia Colombia <intermediacolombia@gmail.com>
2025-11-21 23:07:22 -0500

    Cambio a json la consulta de asistencias general para que sea mas livina ala consulta

--- admin/clients/gets/get_clientes_all.php
+++ admin/clients/gets/get_clientes_all.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../login/session.php';
 require_once __DIR__ . '/../../../inc/config.php';
 $permisopage = 'Ver Clientes';
-include('../login/restriction.php');
+include('../../login/restriction.php');
 
 
 
