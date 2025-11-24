ffbf67009ee0c4ba58fea3dfd4c79d2f3af85190
Intermedia Colombia <intermediacolombia@gmail.com>
2025-11-24 00:58:47 -0500

    Mejora del rendimiento en las consulatas con json

--- admin/bd-today.php
+++ admin/bd-today.php
@@ -1,6 +1,7 @@
 <div class="section-title mb-3">Clientes que cumplen años hoy</div>
 <div class="card-list-wrapper" id="cumple-list">
     <div class="card-list-empty" id="cumple-empty" style="display:none;">
+		<i class="fas fa-check-circle btn-succes" style="font-size:22px;display:block;margin-bottom:6px;"></i>
         No hay cumpleaños hoy.
     </div>
 </div>
