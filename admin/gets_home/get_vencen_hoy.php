6b4846e0383d7646a82345c1c5049f811c6ea5a4
Intermedia Colombia <intermediacolombia@gmail.com>
2025-11-24 00:36:39 -0500

    Mejora del rendimiento en las consulatas con json

--- admin/gets_home/get_vencen_hoy.php
+++ admin/gets_home/get_vencen_hoy.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../login/session.php';
 require_once __DIR__ . '/../../inc/config.php';
 
 date_default_timezone_set('America/Bogota');
-$hoy = date('Y-m-d');
+$hoy = "2025-11-23";
 
 try {
 
