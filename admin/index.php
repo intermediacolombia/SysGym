e342d6e35dafedc4ff53e28d5d059769d0746ab7
Intermedia Colombia <intermediacolombia@gmail.com>
2025-11-25 13:55:32 -0500

    Comparacion ventas hoy y ayer hora a hora

--- admin/index.php
+++ admin/index.php
@@ -536,7 +536,7 @@ require_once __DIR__ . '/../inc/config.php';
       <div class="chart-header">
         <div class="chart-title">Comparativa día semana</div>
         <div class="chart-subtitle">Asistencia hoy VS. <?php setlocale(LC_TIME, 'es_ES.UTF-8', 'Spanish'); 
-			echo strftime("%A"); ?> de la semana pasada </div>
+			echo strftime("%A"); ?> de la semana pasada <span id="hora-ventas"><?= $hora; ?></span></div>
       </div>
       
         <?php include('asis-chart-home-compara.php'); ?>
