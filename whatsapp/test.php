24833dc6844b88e4319afa234b3edba1af639c66
Intermedia Colombia <intermediacolombia@gmail.com>
2026-02-17 14:50:44 -0500

    Test API WS

--- whatsapp/test.php
+++ whatsapp/test.php
@@ -6,7 +6,7 @@
 
 // ==================== CONFIGURACIÓN ====================
 $apiKey = '8veaR5zoXDZYkBRsHfo1jc7xitzKI9Fjy0U';
-$urlEndpoint = 'https://api.360messenger.com/v2/sendMessage';
+$urlEndpoint = 'http://vpn.intermedicolombia.com/api/send';
 
 // DATOS DE PRUEBA (MODIFICA ESTOS)
 $phoneNumber = '573147165269'; // Tu número con código de país (sin + ni espacios)
