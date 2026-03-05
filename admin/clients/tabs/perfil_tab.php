7667db5b7ac88977ebf61694b937e5889b51f64d
Intermedia Colombia <intermediacolombia@gmail.com>
2026-03-05 17:37:50 -0500

    Mejora en estadisticas de asitencias

--- admin/clients/tabs/perfil_tab.php
+++ admin/clients/tabs/perfil_tab.php
@@ -749,7 +749,7 @@
 
 })();
 
-console.log('asistencias:', res.data);
+//console.log('asistencias:', res.data);
 console.log('PAGO_PLAN:', PAGO_PLAN, 'VENCIMIENTO:', VENCIMIENTO);
 console.log('ultimaEnRango:', asistencias.find(a => a.fecha >= PAGO_PLAN && a.fecha <= VENCIMIENTO));
 </script>
